703519a31b8f8a89f8ccb6ac0fad0687a1019b06
Your Name <alexsandrvalov@gmail.com>
2019-12-22 12:04:40 +0200

    add components + navLink

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
-import './Dialogs.module.css'
+import style from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    Коля
+                </div>
+                <div className={style.dialog}>
+                    Вася
+                </div>
+                <div className={style.dialog}>
+                    Петя
+                </div>
+                <div className={style.dialog}>
+                    Дима
+                </div>
+            </div>
+
+            <div className={style.messages}>
+                <div className={style.message}>
+                    Привет
+                </div>
+                <div className={style.message}>
+                    !!!!!!
+                </div>
+                <div className={style.message}>
+                    how are you?
+                </div>
+            </div>
         </div>
     )
 };
